99f1491715e5c2112b353ab3e9b61a3c846e0d76
Lulios <lmai1lot@vivaldi.net>
2023-03-21 08:19:22 -0400

    Ajout du package PathCreator pour le deplacement des canards

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour
+{
+    [SerializeField] private PathCreator pathCreator;
+    public float speed = 2;
+    private float _distanceTravelled;
+    
+    void Update()
+    {
+        _distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(_distanceTravelled);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(_distanceTravelled);
+        transform.eulerAngles -= new Vector3(90, 0, 0);
+    }
+}
